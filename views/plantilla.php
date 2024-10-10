266eeaf1cf8b0cf60179d175442c1347abe209ac
Itss-Josue <josuegomezc21@gmail.com>
2024-10-10 11:21:24 -0500

    init

--- views/plantilla.php
+++ views/plantilla.php
@@ -6,7 +6,7 @@ require_once "./controller/vistas_control.php";
 
 $mostrar = new vistasControlador();
 $vista = $mostrar->obtenerVistaControlador();
-if($vista=="login" || $vista=="perfil" || $vista== "contacto" || $vista== "404"  ){
+if($vista=="login" || $vista=="perfil" || $vista=="contacto" || $vista=="404" || $vista=="carrito" ){
     require_once "./views/".$vista.".php";
     }else{
         include "./views/include/header.php";
