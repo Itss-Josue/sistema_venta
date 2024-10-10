695d1fcbcaaede1feb691e3a22824872a04350a9
Itss-Josue <josuegomezc21@gmail.com>
2024-10-10 11:26:44 -0500

    init

--- model/vistas_model.php
+++ model/vistas_model.php
@@ -2,7 +2,7 @@
 
 class vistaModelo{
     protected static function obtener_vista($vista){
-       $palabras_permitidas =['usuario','calzado','producto','carrito','contacto','detalles_producto','electronica','mejoresmarcas','modahombre','inicio','perfil'];
+       $palabras_permitidas =['usuario','calzado','producto','carrito','detalles_producto','electronica','mejoresmarcas','modahombre','inicio'];
        if (in_array($vista,$palabras_permitidas)){
             if(is_file("./views/".$vista.".php")){
                 $contenido = "./views/".$vista.".php";
@@ -14,8 +14,12 @@ class vistaModelo{
                 $contenido = "404";
             }
 
-       }elseif ($vista=="login" || $vista=="index"){
+         }elseif ($vista=="login" || $vista=="index"){
         $contenido = "login";
+         }elseif ($vista=="perfil" || $vista=="index"){
+        $contenido = "perfil";
+         }elseif ($vista=="contacto" || $vista=="index"){
+        $contenido = "contacto";
 
        }else{
          $contenido = "404";
--- views/plantilla.php
+++ views/plantilla.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once "./config/config.php";
+require_once "./config/config1.php";
 
 require_once "./controller/vistas_control.php";
 
 $mostrar = new vistasControlador();
 $vista = $mostrar->obtenerVistaControlador();
-if($vista=="login" || $vista=="404" ){
+if($vista=="login" || $vista=="contacto" || $vista=="perfil" || $vista=="404" ){
     require_once "./views/".$vista.".php";
     }else{
         include "./views/include/header.php";
